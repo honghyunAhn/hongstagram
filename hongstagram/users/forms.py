f5e828c3e42ae7acdea181db6e47f43add1add0b
honghyunAhn <anh2073@naver.com>
2022-02-22 15:37:20 +0900

    Membership registration/log-in design, template.

--- hongstagram/users/forms.py
+++ hongstagram/users/forms.py
@@ -47,15 +47,12 @@ class SignUpForm(django_forms.ModelForm):
         model = User
         fields = ['email','name','username','password']
         
-        labels = {
-            'email' : '이메일 주소',
-            'name' : '이름',
-            'username' : '사용자 이름',
-            'password' : '비밀번호'
-        }
-        
         widgets = {
             'password': django_forms.PasswordInput(),
+            'email': django_forms.TextInput(attrs={'placeholder': '이메일 주소'}),
+            'name': django_forms.TextInput(attrs={'placeholder': '성명'}),
+            'username': django_forms.TextInput(attrs={'placeholder': '사용자 이름'}),
+            'password': django_forms.PasswordInput(attrs={'placeholder': '비밀번호'}),
         }
         
     def save(self, commit=True):
