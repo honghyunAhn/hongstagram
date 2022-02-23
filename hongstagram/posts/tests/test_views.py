a5954caa1521c729ce2695bb4c5a00efa095373c
honghyunAhn <anh2073@naver.com>
2022-02-23 13:37:29 +0900

    Post registration screen - form

--- hongstagram/posts/tests/test_views.py
+++ hongstagram/posts/tests/test_views.py
@@ -1,12 +1,47 @@
+from urllib import response
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 class TestPosts(TestCase):
-    
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            username = "anh288",
+            email = "anh2073@naver.com",
+            password = "1234"
+        )
+        
     def test_get_posts_page(self):
         url = reverse("posts:post_create")
         response = self.client.get(url)
         
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "posts/post_create.html")
+        
+    def test_post_creating_posts(self):
+        login = self.client.login(username = "anh288", password = "1234")
+        self.assertTrue(login)
+        
+        url = reverse("posts:post_create")
+        image = SimpleUploadedFile("test.jpg", b"whatevercontents")
+        response = self.client.post(
+            url,
+            {"image":image, "caption":"test test"}
+        )
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "posts/base.html")
+        
+    def test_post_posts_create_not_login(self):
+        url = reverse("posts:post_create")
+        image = SimpleUploadedFile("test.jpg", b"whatevercontents")
+        response = self.client.post(
+            url,
+            {"image":image, "caption":"test test"}
+        )
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/main.html")
--- hongstagram/posts/views.py
+++ hongstagram/posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from hongstagram.posts.forms import CreatePostForm
 from hongstagram.users.models import User as user_model
 
 from . import models
+from .forms import CreatePostForm
 
 # Create your views here.
 def index(request):
@@ -9,22 +11,31 @@ def index(request):
 
 def post_create(request):
     if request.method == 'GET':
-        return render(request, 'posts/post_create.html')
+        form = CreatePostForm()
+        return render(request, 'posts/post_create.html', {'form': form})
 
     elif request.method == 'POST':
         if request.user.is_authenticated:
             user = get_object_or_404(user_model, pk=request.user.id)
-            image = request.FILES['image']
-            caption = request.POST['caption']
+            # image = request.FILES['image']
+            # caption = request.POST['caption']
 
-            new_post = models.Post.objects.create(
-                author = user,
-                image = image,
-                caption = caption
-            )
-            new_post.save()
+            # new_post = models.Post.objects.create(
+            #     author = user,
+            #     image = image,
+            #     caption = caption
+            # )
+            # new_post.save()
+            
+            form = CreatePostForm(request.POST, request.FILES)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = user
+                post.save()
+            else:
+                print(form.errors)
 
-            return render(request, 'post/base.html')
+            return render(request, 'posts/main.html')
 
         else:
             return render(request, 'users/main.html')
