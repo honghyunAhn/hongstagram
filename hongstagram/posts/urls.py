d3c51c84adfbcb265e3a3cbb2d325aec8ef596dd
honghyunAhn <anh2073@naver.com>
2022-02-24 13:28:53 +0900

    Post comment generation - django form, api development

--- hongstagram/posts/urls.py
+++ hongstagram/posts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name = "posts"
+app_name = 'posts'
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("create/", views.post_create, name='post_create'),
+    # /posts/
+    path('', views.index, name='index'),
+
+    # /posts/create/
+    path('create/', views.post_create, name='post_create'),
+
+    # /posts/1/comment_create/
+    path('<int:post_id>/comment_create', views.comment_create, name="comment_create"),
 ]
--- hongstagram/posts/views.py
+++ hongstagram/posts/views.py
@@ -1,30 +1,36 @@
-from django.shortcuts import render, get_object_or_404
-from yaml import serialize
-from hongstagram.posts.forms import CreatePostForm
-from hongstagram.users.models import User as user_model
 from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 
+from hongstagram.users.models import User as user_model
 from . import models, serializers
-from .forms import CreatePostForm
+from .forms import CreatePostForm, CommentForm
 
 # Create your views here.
 def index(request):
     if request.method == "GET":
         if request.user.is_authenticated:
+            comment_form = CommentForm()
+
             user = get_object_or_404(user_model, pk=request.user.id)
             following = user.following.all()
             posts = models.Post.objects.filter(
                 Q(author__in=following) | Q(author=user)
             )
-            
+
             serializer = serializers.PostSerializer(posts, many=True)
-            
-            return render(request, 'posts/main.html', {"posts":serializer.data})
+            print(serializer.data)
+
+            return render(
+                request,
+                'posts/main.html',
+                {"posts": serializer.data, "comment_form": comment_form}
+            )
 
 def post_create(request):
     if request.method == 'GET':
         form = CreatePostForm()
-        return render(request, 'posts/post_create.html', {'form': form})
+        return render(request, 'posts/post_create.html', {"form": form})
 
     elif request.method == 'POST':
         if request.user.is_authenticated:
@@ -38,7 +44,7 @@ def post_create(request):
             #     caption = caption
             # )
             # new_post.save()
-            
+
             form = CreatePostForm(request.POST, request.FILES)
             if form.is_valid():
                 post = form.save(commit=False)
@@ -48,6 +54,22 @@ def post_create(request):
                 print(form.errors)
 
             return render(request, 'posts/main.html')
+
+        else:
+            return render(request, 'users/main.html')
+
+def comment_create(request, post_id):
+    if request.user.is_authenticated:
+        post = get_object_or_404(models.Post, pk=post_id)
+
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.posts = post
+            comment.save()
+
+            return redirect(reverse('posts:index') + "#comment-"+ str(comment.id))
 
         else:
             return render(request, 'users/main.html')
